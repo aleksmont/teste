a2866ab3805d51cf0884eaba58a7decbff6c91ba
Aleksander Monteiro <aleksmont@gmail.com>
2025-10-14 20:51:04 -0300

    find

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @profile = Profile.new
+    q = Profile
+    if params[:search]
+      q = q.where("concat_ws(' ', profiles.name, profiles.github_username, profiles.github_organization, profiles.github_location) ILIKE ?", "%#{params[:search]}%")
+    end
+
+    @profiles = q
+                  .page(params[:page] || 1)
+                  .per(params[:per_page] || 5)
+                  .order(params[:order_by] || "name ASC")
   end
 end
